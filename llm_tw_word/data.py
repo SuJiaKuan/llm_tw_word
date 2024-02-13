9e2ec335870e0e6f773705571d03ecc51ac3d390
SuJiaKuan <feabries@gmail.com>
2024-02-13 15:11:34 +0800

    Add a function to generate (traditionla, taiwanese) data

---
+++ llm_tw_word/data.py
@@ -0,0 +1,20 @@
+import requests
+
+
+def _request_fanhuaji(
+    text,
+    converter,
+    url="https://api.zhconvert.org/convert",
+):
+    return requests.post(url, data={
+        "text": text,
+        "converter": converter,
+    }).json()["data"]["text"]
+
+
+
+def simp2data(text_simp):
+    text_trad = _request_fanhuaji(text_simp, "Traditional")
+    text_tw = _request_fanhuaji(text_trad, "Taiwan")
+
+    return (text_trad, text_tw)
